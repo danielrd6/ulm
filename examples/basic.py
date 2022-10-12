08797145f3971303da67cc319142f23f75993513
danielrd6 <daniel@astro.ufsc.br>
2022-10-12 09:46:15 -0300

    Fixed first conditional for projection selection
    Also implemented the conditional block for plotting.

--- examples/basic.py
+++ examples/basic.py
@@ -17,20 +17,13 @@ def draw_rectangle_3d(center, length, axes, **kwargs):
             axes.plot(v[:, 0], v[:, 1], v[:, 2], **kwargs)
 
 
-def main(n_particles : int = 4, plot: bool = True, projection="2d"):
+def main(n_particles: int = 4, plot: bool = True, projection="2d"):
     if projection == "3d":
         pos = initial_conditions.generate_volume(n=n_particles, make_3d=True)
-    if projection == "2d":
+    elif projection == "2d":
         pos = initial_conditions.generate_volume(n=n_particles, make_3d=False)
     else:
         raise RuntimeError
-    # r = []
-    # for i in range(pos.shape[0]):
-    #     for j in range(pos.shape[0]):
-    #         if i != j:
-    #             r.append(pos[i] - pos[j])
-    # r = np.linalg.norm(r, axis=1)
-    # print(f"min distance = {r.min()}")
 
     mass = np.ones(pos.shape[0])
     particle_id = np.arange(pos.shape[0])
@@ -40,37 +33,40 @@ def main(n_particles : int = 4, plot: bool = True, projection="2d"):
     t = tree_generator.Tree(particles=particles)
     t.make_tree(verbose=True)
 
-    fig = plt.figure()
-    if projection == "3d":
-        ax = fig.add_subplot(projection=projection)
-    elif projection == "2d":
-        ax = fig.add_subplot()
-    else:
-        raise RuntimeError
+    if plot:
+        fig = plt.figure()
+        if projection == "3d":
+            ax = fig.add_subplot(projection=projection)
+        elif projection == "2d":
+            ax = fig.add_subplot()
+        else:
+            raise RuntimeError
 
-    def draw_boundaries(d, axes, proj):
-        if d.branch:
-            for e in d.children:
-                if projection == "2d":
-                    rect = patches.Rectangle((e.center[0] - (e.length / 2), e.center[1] - (e.length / 2)),
-                                             width=e.length, height=e.length, alpha=0.1, facecolor="none",
-                                             edgecolor="black")
-                    axes.add_patch(rect)
-                elif projection == "3d":
-                    draw_rectangle_3d(d.center, d.length, axes, color="black", lw=1, alpha=0.1)
-                else:
-                    raise RuntimeError
-                draw_boundaries(e, axes, proj)
+        def draw_boundaries(d, axes, proj):
+            if d.branch:
+                for e in d.children:
+                    if projection == "2d":
+                        rect = patches.Rectangle((e.center[0] - (e.length / 2), e.center[1] - (e.length / 2)),
+                                                 width=e.length, height=e.length, alpha=0.1, facecolor="none",
+                                                 edgecolor="black")
+                        axes.add_patch(rect)
+                    elif projection == "3d":
+                        draw_rectangle_3d(d.center, d.length, axes, color="black", lw=1, alpha=0.1)
+                    else:
+                        raise RuntimeError
+                    draw_boundaries(e, axes, proj)
 
-    for a in t.cubes:
-        draw_boundaries(a, ax, proj=projection)
+        for a in t.cubes:
+            draw_boundaries(a, ax, proj=projection)
 
-    if projection == "2d":
-        ax.scatter(particles["x"], particles["y"], marker=".")
-    elif projection == "3d":
-        ax.scatter(particles["x"], particles["y"], particles["z"], marker=".")
+        if projection == "2d":
+            ax.scatter(particles["x"], particles["y"], marker=".")
+        elif projection == "3d":
+            ax.scatter(particles["x"], particles["y"], particles["z"], marker=".")
+        else:
+            raise RuntimeError
 
-    plt.show()
+        plt.show()
 
     return t
 
